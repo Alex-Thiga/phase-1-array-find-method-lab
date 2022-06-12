37e73258b740547befd00c67b1a734775fb6cd97
Alex Thiga <loydeflacko@gmail.com>
2022-06-12 13:55:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin ( arrayName )
+{
+    const result = arrayName.find( arrayName => arrayName.result === 'W' )
+    return !!result ? result.year : undefined
+}
